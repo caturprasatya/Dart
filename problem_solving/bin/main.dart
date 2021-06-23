cd8ec1ad4561a28ef15e4cb9edcdcf06e7fcdadc
caturprasatya <caturprasatya1224@gmail.com>
2021-06-23 17:39:42 +0700

    solve grade student

--- problem_solving/bin/main.dart
+++ problem_solving/bin/main.dart
@@ -1,7 +1,32 @@
 import 'dart:io';
+import 'dart:math';
 
 void main(List<String> arguments) {
-  stdout.write('Siapa kamu: ');
-  var nama = stdin.readLineSync();
-  print('Hello $nama!');
+  stdout.write('Nama Guru Pengampu: ');
+  var namaGuru = stdin.readLineSync();
+  stdout.write('Nama Siswa Yang Akan Dinilai: ');
+  var namaSiswa = stdin.readLineSync();
+  Random random = Random();
+
+  int nilaiSiswa = random.nextInt(101);
+
+  String grade = '';
+
+  if (nilaiSiswa <= 100 && nilaiSiswa >= 80) {
+    grade = 'A';
+  } else if (nilaiSiswa < 80 && nilaiSiswa >= 65) {
+    grade = 'B';
+  } else if (nilaiSiswa < 65 && nilaiSiswa >= 50) {
+    grade = 'C';
+  } else if (nilaiSiswa < 50 && nilaiSiswa >= 35) {
+    grade = 'D';
+  } else if (nilaiSiswa < 35 && nilaiSiswa >= 0) {
+    grade = 'E';
+  } else {
+    print('Invaid Data');
+  }
+
+  print('Guru Pengampu: $namaGuru');
+  print('Nama Siswa: $namaSiswa');
+  print('Nilai Siswa: $nilaiSiswa => Garde $grade');
 }
